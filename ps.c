4a747c23a49b40030e9fe2d007c8fe5c6488f9e8
Salomon Avila <104867468+SalomonAvila@users.noreply.github.com>
2025-05-07 21:46:47 +0000

    Avance

--- ps.c
+++ ps.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+typedef struct {
+    char tipo;
+    char nombreLibro[100];
+    long ISBN;
+} Peticion;
 
 void menuGeneral(){
     printf("De donde quiere obtener su archivo\n");
@@ -14,23 +22,33 @@ void menu(){
 }
 
 
-int main() {
-    printf("Bienvenido al programa\n Codigo de proceso: %d\n", getpid());
-    int ans = -1;
-    int ansInt = 0;
-    char nomArch[100];
-    while(ans != 0){
-        menuGeneral();
-        scanf("%d",&ans);
-        getchar();
-        switch (ans){
-            case 1:
-                menu();
-                break;
-            case 2:
-                fgets(nomArch,sizeof(nomArch),stdin);
+int main(int argc, char *argv[]) {
+    char miPipe[100];
+    //printf("El argc es de: %d\n",argc);
+    if(argc == 3){
+        //printf("El argv[1] es de: %s\n",argv[1]);
+        if(argv[1][0] == '-' && argv[1][1] == 'p'){
+            strcpy(miPipe, argv[2]);
+            int res = access(miPipe, F_OK);
+            //printf("El resultado de la busqueda con access es: %d\n", res);
+            if(res == 0){
+                //printf("Ya existe\n");
+                //Enviar la informacion
+                // No le corresponde a ps elimuinar un pipe unlink(miPipe);
+            }else{
+                printf("No existe el pipe, recuerde que tiene que ser igual al proceso receptor");
+                //mkfifo(miPipe,0777);
+            }
+        }else{
+            printf("Comando erroneo, debería ser solicitante [–i file] –p pipeReceptor");
         }
+    }else if(argc == 5){
+        
+    }else{
+        printf("Comando erroneo, debería ser solicitante [–i file] –p pipeReceptor");
+        return 0;
     }
-
-    return 0;
+    while(true){
+        
+    }
 }
