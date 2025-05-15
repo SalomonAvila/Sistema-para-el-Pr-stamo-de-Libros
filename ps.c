dcff7ce70063c4f9f9b0af918b9c363632e366c1
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-15 11:58:34 -0500

    Avance pipes

--- ps.c
+++ ps.c
@@ -1,54 +1,119 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdbool.h>
 
-typedef struct {
+typedef struct
+{
     char tipo;
     char nombreLibro[100];
     long ISBN;
 } Peticion;
 
-void menuGeneral(){
+void menuGeneral()
+{
     printf("De donde quiere obtener su archivo\n");
     printf("1. A traves de un menu");
     printf("2. Mediante un archivo de texto\n");
 }
 
-void menu(){
+void menu()
+{
     printf("Menu de opciones\n");
 }
 
- 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     char miPipe[100];
-    //printf("El argc es de: %d\n",argc);
-    if(argc == 3){
-        //printf("El argv[1] es de: %s\n",argv[1]);
-        if(argv[1][0] == '-' && argv[1][1] == 'p'){
-            strcpy(miPipe, argv[2]);
-            int res = access(miPipe, F_OK);
-            //printf("El resultado de la busqueda con access es: %d\n", res);
-            if(res == 0){
-                //printf("Ya existe\n");
-                //Enviar la informacion
-                // No le corresponde a ps elimuinar un pipe unlink(miPipe);
-            }else{
-                printf("No existe el pipe, recuerde que tiene que ser igual al proceso receptor");
-                //mkfifo(miPipe,0777);
-            }
-        }else{
-            printf("Comando erroneo, debería ser solicitante [–i file] –p pipeReceptor");
-        }
-    }else if(argc == 5){
-        
-    }else{
-        printf("Comando erroneo, debería ser solicitante [–i file] –p pipeReceptor");
-        return 0;
-    }
-    while(true){
 
+    if (argc == 3)
+    {
+        if (argv[1][0] == '-' && argv[1][1] == 'p')
+        {
+            strcpy(miPipe, argv[2]);
+            if (access(miPipe, F_OK) != 0)
+            {
+                fprintf(stderr, "Error: El pipe '%s' no existe. Asegúrese de que el proceso receptor esté en ejecución.\n", miPipe);
+                return 1;
+            }
+        }
+        else
+        {
+            printf("Comando erroneo, debería ser solicitante [–i file] –p pipeReceptor\n");
+            return 1;
+        }
+    }
+    else
+    {
+        printf("Comando erroneo, debería ser solicitante [–i file] –p pipeReceptor\n");
+        return 1;
+    }
+
+    while (true)
+    {
+        menuGeneral();
+        int opcionUno, opcionDos;
+        Peticion peticion;
+        scanf("%d", &opcionUno);
+        switch (opcionUno)
+        {
+        case 1:
+            menu();
+            scanf("%d", &opcionDos);
+            switch (opcionDos)
+            {
+            case 1:
+                printf("Devolviendo un libro\n");
+                scanf("%s", peticion.nombreLibro);
+                printf("El nombre es: %s \n", peticion.nombreLibro);
+                scanf("%ld", &peticion.ISBN);
+                printf("El ISBN es: %ld \n", peticion.ISBN);
+                scanf(" %c", &peticion.tipo);
+                printf("El tipo es: %c \n", peticion.tipo);
+
+                int fd = open(miPipe, O_WRONLY);
+                if (fd == -1)
+                {
+                    perror("Error al abrir el pipe para escritura");
+                    break;
+                }
+
+                if (write(fd, &peticion, sizeof(Peticion)) == -1)
+                {
+                    perror("Error al escribir en el pipe");
+                }
+                else
+                {
+                    printf("Se ha escrito en el pipe\n");
+                }
+                close(fd);
+                break;
+
+            case 2:
+                printf("Renovando un libro\n");
+                break;
+
+            case 3:
+                printf("Solicitando un libro\n");
+                break;
+
+            default:
+                printf("Opcion no valida\n");
+                break;
+            }
+            break;
+
+        case 2:
+            printf("Opcion no implementada\n");
+            break;
+
+        default:
+            printf("Opcion no valida\n");
+            break;
+        }
     }
 }
--- rp.c
+++ rp.c
@@ -1,38 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdbool.h>
 #include <pthread.h>
 
-typedef struct {
+typedef struct
+{
     char tipo;
     char nombreLibro[100];
     long ISBN;
 } Peticion;
- 
+
 Peticion buffer[11];
 
-void imprimirRegistro(){
-    FILE* registros = fopen("registros.txt", "w");
-
+void imprimirRegistro()
+{
+    FILE *registros = fopen("registros.txt", "w");
 }
 
-void *recibirComandos(char comando, pthread_t pt){
-    if(comando == 's'){
+void *recibirComandos(char comando, pthread_t pt)
+{
+    if (comando == 's')
+    {
         pthread_exit(pthread_self());
         pthread_exit(pt);
-    }else if(comando == 'r'){
-
+    }
+    else if (comando == 'r')
+    {
     }
 }
 
-void *operaciones(Peticion peticion){
-
+void *operaciones(Peticion peticion)
+{
+    printf("El tipo de peticion es: %c\n", peticion.tipo);
 }
 
-int main(int argc, char *argv[]) {
-    pthread_t auxiliar1,auxiliar2;
-    pthread_create(auxiliar1,NULL,operaciones,NULL);
-    pthread_create(auxiliar2,NULL,recibirComandos,NULL);
+int main(int argc, char *argv[])
+{
+    pthread_t auxiliar1, auxiliar2;
+    printf("El argc es de: %d\n", argc);
+    mkfifo(argv[2], 0666);
+    int fd = open(argv[2], O_RDONLY);
+    if (fd == -1) {
+        perror("Error al abrir el pipe para lectura");
+        return 1;
+    }
+    pthread_create(&auxiliar1, NULL, operaciones, NULL);
+    pthread_create(&auxiliar2, NULL, recibirComandos, NULL);
+    Peticion peticion;
+    peticion.ISBN = -1;
+    while (true)
+    {
+        ssize_t bytesLeidos = read(fd, &peticion, sizeof(peticion));
+        if (bytesLeidos > 0 && peticion.ISBN != -1)
+        {
+            printf("El ISBN es: %ld\n", peticion.ISBN);
+            printf("El tipo de peticion es: %c\n", peticion.tipo);
+            printf("El nombre del libro es: %s\n", peticion.nombreLibro);
+            printf("El ISBN del libro es: %ld\n", peticion.ISBN);
+        }
+    }
 }
